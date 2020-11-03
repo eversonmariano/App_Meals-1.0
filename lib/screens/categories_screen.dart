4d6f0a675731285bb826e20e36597790fd52ecb6
Japa-PB <mariano.computacao@gmail.com>
2020-11-02 23:29:47 -0300

    App feito em Dart/Flutter

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//import 'package:meals/components/category_item.dart';
+//import 'package:meals/models/category.dart';
+
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 350,
+        childAspectRatio: 2.1,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES.map((cat) {
+        return CategoryItem(cat);
+      }).toList(),
+    );
+  }
+}
